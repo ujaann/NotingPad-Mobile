fd4b7700f973e5a0b399a110c34a2236dec5888f
ujaann <ujanst@gmail.com>
2024-01-11 09:41:06 +0545

    Created model using quicktype

--- lib/firestore_example/firestore_example.dart
+++ lib/firestore_example/firestore_example.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:notingpad/model/user_model.dart';
 import 'package:overlay_kit/overlay_kit.dart';
 
@@ -17,6 +21,34 @@ class _FirestoreExampleState extends State<FirestoreExample> {
   TextEditingController lname = TextEditingController();
   final firestore = FirebaseFirestore.instance;
   bool loading = false;
+
+  ImagePicker picker = ImagePicker();
+  FirebaseStorage storage = FirebaseStorage.instance;
+  File ? image;
+  String ? url;
+
+  void pickImage(ImageSource source) async {
+    var selected = await picker.pickImage(source: source, imageQuality: 100);
+    if (selected == null) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("No image selected")));
+    } else {
+      setState(() {
+        image=File(selected.path);
+        print(image!.path.split('/'));
+        saveToStorage();
+      });
+    }
+  }
+
+  void saveToStorage() async{
+    String name =image!.path.split('/').last;
+    final photo=await storage.ref().child('users').child(name).putFile(File(image!.path));
+    String tempUrl= await photo.ref.getDownloadURL();
+    setState(() {
+      url=tempUrl;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +68,68 @@ class _FirestoreExampleState extends State<FirestoreExample> {
           TextFormField(
             controller: lname,
           ),
+
+          image==null?SizedBox():Image.file(image!, height: 200,width: 200),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: Text("Choose image"),
+                          content: Container(
+                            height: 150,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    child: InkWell(
+                                      onTap: (){
+                                        pickImage(ImageSource.camera);
+                                        Navigator.pop(context);
+                                      },
+                                      child: Column(
+                                  children: [
+                                      Image.asset(
+                                        "assets/images/camera.png",
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                      Text("Camera")
+                                  ],
+                                ),
+                                    )),
+                                Expanded(
+                                    child: InkWell(
+                                    onTap:() {
+                                      pickImage(ImageSource.gallery);
+                                      Navigator.pop(context);
+                                    },
+                                      child: Column(
+                                  children: [
+                                      Image.asset(
+                                        "assets/images/gallery.png",
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                      Text("Gallery")
+                                  ],
+                                ),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ));
+              },
+              child: Text("Hlo img")),
           ElevatedButton(
               onPressed: () async {
                 OverlayLoadingProgress.start();
 
-                UserModel model=UserModel(email: email.text,firstname: fname.text,lastname: lname.text);
+                UserModel model = UserModel(
+                    email: email.text,
+                    firstname: fname.text,
+                    lastname: lname.text,
+                    image: url
+                );
                 await firestore
                     .collection('users')
                     .doc()
@@ -49,13 +138,17 @@ class _FirestoreExampleState extends State<FirestoreExample> {
                   email.clear();
                   fname.clear();
                   lname.clear();
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text("Data Submitted"),backgroundColor: Colors.lightGreen,));
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Data Submitted"),
+                    backgroundColor: Colors.lightGreen,
+                  ));
                   OverlayLoadingProgress.stop();
                 }).onError((error, stackTrace) {
                   OverlayLoadingProgress.stop();
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text(error.toString()),backgroundColor: Colors.deepOrange,));
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(error.toString()),
+                    backgroundColor: Colors.deepOrange,
+                  ));
                 });
               },
               child: Text("Submit")),
--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -12,22 +12,26 @@ class UserModel {
   String? email;
   String? firstname;
   String? lastname;
+  String ? image;
 
   UserModel({
     required this.email,
     this.firstname,
     this.lastname,
+    this.image
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         email: json["email"],
         firstname: json["firstname"],
         lastname: json["lastname"],
+        image: json["image"],
       );
 
   Map<String, dynamic> toJson() => {
         "email": email,
         "firstname": firstname,
         "lastname": lastname,
+        "image": image,
       };
 }
